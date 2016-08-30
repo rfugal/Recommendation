e00cfd52c92fd6d3d26a7d19f3d0dc70deba0cf6
rfugal <rfugal@gmail.com>
2016-08-30 16:46:00 -0600

    Add New Book
    Still buggy

--- words.js
+++ words.js
@@ -1,7 +1,7 @@
 function WordsCtrl($scope) {
     
     if (localStorage.getItem('SaraWordModel') != null) {
-        $scope.Words = JSON.parse(localStorage.getItem('SaraWordModel'));
+        $scope.Words = angular.fromJson(localStorage.getItem('SaraWordModel'));
     } else {
         $scope.Words = {
             'this': {RAN:true, encounters:0},
@@ -45,20 +45,21 @@ function WordsCtrl($scope) {
             'reading': {RAN:true, encounters:0},
             'my': {RAN:true, encounters:0}
         };
+        localStorage.setItem('SaraWordModel', angular.toJson($scope.Words));
     };
     
     $scope.getFamiliarWords = function () {
         var RANcount = 0;
-        for (word in $scope.Words) {
-            if ($scope.Words[word].RAN) {RANcount++};
+        for (w in $scope.Words) {
+            if ($scope.Words[w].RAN == true) {RANcount++};
         };
-        localStorage.setItem('SaraWordModel', JSON.stringify($scope.Words));
         return RANcount;
     };
     
     
     $scope.addWords = function () {
         var newWords = $scope.formInputText.toLowerCase().match(/\b[A-z']+/gi);
+        $scope.Words = angular.fromJson(localStorage.getItem('SaraWordModel'));
         $scope.formInputText = '';
         for (i=0; i<newWords.length; i++) {
             if ($scope.Words[newWords[i]] === undefined) {
@@ -70,5 +71,66 @@ function WordsCtrl($scope) {
                 };
             };
         };
+        localStorage.setItem('SaraWordModel', angular.toJson($scope.Words));
+    };
+}
+
+var bookLibrary = {};
+
+if (localStorage.getItem('SaraBookLibrary') != null) {
+    bookLibrary = angular.fromJson(localStorage.getItem('SaraBookLibrary'));
+};
+
+function addBooksCtrl($scope) {
+    $scope.addBookRead = "Read";
+    $scope.newBookWords = [];
+    $scope.addBook = function () {
+        $scope.newBookWords = $scope.addBookText.toLowerCase().match(/\b[A-z']+/gi);
+        alert($scope.newBookWords)
+        $scope.newBookWords = _.countBy($scope.newBookWords, function(w) {
+            return w;
+        });
+        var bookName = $scope.addBookAuthor.toLowerCase().match(/\b[A-z']+/gi);
+        bookName = bookName[bookName.length-1]+$scope.addBookYear+$scope.addBookTitle.toLowerCase().match(/\b[A-z']+/gi)[0];
+        bookLibrary[bookName] = {
+            title: $scope.addBookTitle,
+            author: $scope.addBookAuthor,
+            year: $scope.addBookYear,
+            publisher: $scope.addBookPublisher,
+            words: $scope.newBookWords
+        };
+        localStorage.setItem('SaraBookLibrary', angular.toJson(bookLibrary));
+        
+        if ($scope.addBookRead != "Unread") {
+            var wordModel = angular.fromJson(localStorage.getItem('SaraWordModel'));
+            //add 1 encounter to each word
+            for (word in $scope.newBookWords) {
+                if (wordModel[word] === undefined) {
+                    wordModel[word] = {RAN:false, encounters: 1};
+                } else {
+                    wordModel[word].encounters++;
+                };
+            }
+            if ($scope.addBookRead == "Fluent") {
+                for (word in $scope.newBookWords) {
+                    wordModel[word].RAN = true;
+                };
+            } else if ($scope.addBookRead == "Read") {
+                //make words with > 3 encounters RAN:true
+                for (word in $scope.newBookWords) {
+                    if (wordModel[word].encounters > 3) {
+                        wordModel[word].RAN = true;
+                    };
+                };
+            };
+            localStorage.setItem('SaraWordModel', angular.toJson(wordModel));
+        };
+        
+        $scope.addBookText = '';
+        $scope.addBookTitle = '';
+        $scope.addBookAuthor = '';
+        $scope.addBookYear = '';
+        $scope.addBookPublisher = '';
+        
     };
 }
